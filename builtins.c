3dd98fd741fd609e16a173ae3d5a95d3e4ade61b
milescajus <80205080+milescajus@users.noreply.github.com>
2022-06-07 17:23:17 +0200

    not even sure

--- builtins.c
+++ builtins.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+
+#include "shell.h"
 
 int help(char **args)
 {
@@ -9,5 +13,11 @@ int help(char **args)
 
 int cd(char **args)
 {
+    if (strcmp(args[1], "-") == 0) {
+        args[1] = getenv("OLDPWD");
+    }
+
+    setenv("OLDPWD", getenv("PWD"), 1);
+
     return chdir(args[1]);
 }
