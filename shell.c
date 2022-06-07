0814f410d459441d2a3e5bed4899f06774373339
milescajus <80205080+milescajus@users.noreply.github.com>
2022-06-07 12:40:20 +0200

    added rudimentary tokenizing

--- shell.c
+++ shell.c
@@ -1,18 +1,31 @@
 #include "shell.h"
 #define SIZE 256
 
+char **tokenize(char * input)
+{
+    char **args = (char **)calloc(sizeof(char *), SIZE);
+    int i = 0;
+    while ((args[i] = strsep(&input, " ")) != NULL) { i++; }
+    args[i] = NULL;
+    args = realloc(args, sizeof(char *) * i);
+
+    return args;
+}
+
 int main()
 {
     pid_t pid;
 
     while (1) {
-        char line[SIZE];
+        char *line = (char *)calloc(sizeof(char), SIZE);
         void *in = fgets(line, SIZE, stdin);
 
         if (in == NULL)
             return 0;
 
-        char *args[] = {line, NULL};
+        line[strlen(line) - 1] = '\0';
+
+        char **args = tokenize(line);
 
         pid = fork();
 
@@ -29,6 +42,8 @@ int main()
             wait(NULL);
         }
 
+        free(line);
+        free(args);
     }
 
     return -1;
