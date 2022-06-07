7f999171c61cba0e40ebbe7ea80d218c63a753b8
milescajus <80205080+milescajus@users.noreply.github.com>
2022-06-07 11:44:04 +0200

    better forking

--- shell.c
+++ shell.c
@@ -8,21 +8,25 @@ int main()
     while (1) {
         char line[SIZE];
         void *in = fgets(line, SIZE, stdin);
+
         if (in == NULL)
             return 0;
 
         char *args[] = {line, NULL};
+
         pid = fork();
-        switch (pid) {
-            case -1: // FAILED
-                fprintf(stderr, "FORK FAILED");
-                return -1;
-            case 0: // CHILD PROC
-                execvp(line, args);
-                break;
-            default: // PARENT
-                wait(NULL);
-                break;
+
+        if (pid < 0) {
+            fprintf(stderr, "fork failed\n");
+            break;
+        } else if (pid == 0) {
+            if (execvp(line, args) < 0) {
+                fprintf(stderr, "exec failed\n");
+                perror(line);
+            }
+            break;
+        } else {
+            wait(NULL);
         }
 
     }
