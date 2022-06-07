27369161fed0e650f23643f758e34e6d70724695
milescajus <80205080+milescajus@users.noreply.github.com>
2022-06-07 13:42:19 +0200

    refactored for modularity

--- shell.c
+++ shell.c
@@ -1,6 +1,52 @@
 #include "shell.h"
 #define SIZE 256
 
+pid_t pid;
+char *cmd;      // dynamically allocated
+char **args;    // dynamically allocated
+void *in;
+
+int main()
+{
+    int res = loop();
+
+    free(cmd);
+    free(args);
+    printf("bye!\n");
+    return res;
+}
+
+int loop()
+{
+    while (1) {
+        printf("🍕> ");
+        cmd = (char *)calloc(sizeof(char), SIZE);
+        in = fgets(cmd, SIZE, stdin);
+
+        if (in == NULL)
+            break;
+
+        cmd[strlen(cmd) - 1] = '\0';
+
+        args = tokenize(cmd);
+
+        pid = fork();
+
+        if (pid < 0) {
+            fprintf(stderr, "fork failed\n");
+            break;
+        } else if (pid == 0) {
+            execute();
+            break;
+        } else {
+            wait(NULL);
+        }
+
+    }
+
+    return 0;
+}
+
 char **tokenize(char * input)
 {
     char **args = (char **)calloc(sizeof(char *), SIZE);
@@ -11,43 +57,10 @@ char **tokenize(char * input)
     return args;
 }
 
-int main()
+void execute()
 {
-    pid_t pid;
-    char *line;
-    void *in;
-    char **args;
-
-    while (1) {
-        printf("🍕> ");
-        line = (char *)calloc(sizeof(char), SIZE);
-        in = fgets(line, SIZE, stdin);
-
-        if (in == NULL)
-            break;
-
-        line[strlen(line) - 1] = '\0';
-
-        args = tokenize(line);
-
-        pid = fork();
-
-        if (pid < 0) {
-            fprintf(stderr, "fork failed\n");
-            break;
-        } else if (pid == 0) {
-            if (execvp(line, args) < 0) {
-                fprintf(stderr, "exec failed\n");
-                perror(line);
-            }
-            break;
-        } else {
-            wait(NULL);
-        }
-
+    if (execvp(cmd, args) < 0) {
+        fprintf(stderr, "exec failed\n");
+        perror(cmd);
     }
-
-    free(line);
-    free(args);
-    return 0;
 }
--- shell.h
+++ shell.h
@@ -2,3 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+char **tokenize(char * input);
+void execute(char * cmd, char ** args);
+int loop();
